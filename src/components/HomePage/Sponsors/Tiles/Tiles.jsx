d2aa46b74744fb2b80c34d3202238f800adec224
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-07-16 16:55:00 -0400

    scroll animation mobile

--- src/components/HomePage/Sponsors/Tiles/Tiles.jsx
+++ src/components/HomePage/Sponsors/Tiles/Tiles.jsx
@@ -1,60 +1,62 @@
 'use client';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './Tiles.module.scss';
 import Image from 'next/image';
 import clsx from 'clsx';
 import useWindowSize from '@/hooks/useWindowSize';
 
 export default function Tiles({ sponsors }) {
-  const scrollRef = useRef(null);
-  const itemsRef = useRef([]);
+  const containerRef = useRef(null);
+  const [offset, setOffset] = useState(0);
 
   const { isMobile } = useWindowSize();
 
-  // Scroll animation
+  // scroll animation
   useEffect(() => {
-    const container = scrollRef.current;
-    if (!container) return;
-
-    let animationFrameId;
     const speed = 0.5;
+    let animationFrameId;
 
     const animate = () => {
-      container.scrollLeft += speed;
+      setOffset(prev => {
+        const container = containerRef.current;
+        if (!container) return prev;
 
-      const singleContentWidth = container.scrollWidth / 2;
-      if (container.scrollLeft >= singleContentWidth) {
-        container.scrollLeft -= singleContentWidth;
-      }
+        const singleContentWidth = container.scrollWidth / 2;
+
+        const next = prev + speed;
+        return next >= singleContentWidth ? 0 : next;
+      });
       animationFrameId = requestAnimationFrame(animate);
     };
 
     animationFrameId = requestAnimationFrame(animate);
+
     return () => cancelAnimationFrame(animationFrameId);
   }, []);
 
-  // Duplicate sponsors for infinite scroll
   const sponsorsWithDuplicates = !isMobile
     ? [...sponsors, ...sponsors]
     : sponsors;
 
   return (
-    <div className={styles.scrollContainer} ref={scrollRef}>
-      {sponsorsWithDuplicates.map((mediaItem, index) => {
-        return (
-          <div
-            key={`${mediaItem.src}-${index}`}
-            className={clsx(styles.logo)}
-            ref={el => (itemsRef.current[index] = el)}
-          >
+    <div className={styles.outerContainer}>
+      <div
+        className={styles.innerScrollContainer}
+        ref={containerRef}
+        style={{
+          transform: `translateX(-${offset}px)`,
+        }}
+      >
+        {sponsorsWithDuplicates.map((mediaItem, index) => (
+          <div key={`${mediaItem.src}-${index}`} className={clsx(styles.logo)}>
             <Image
               src={mediaItem.src}
               alt={mediaItem.alt}
               className={styles.image}
             />
           </div>
-        );
-      })}
+        ))}
+      </div>
     </div>
   );
 }
